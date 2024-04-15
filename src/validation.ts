4686bf2dfb6700b96a82e5fd340a22ba101cb4e5
kiruse <26940742+Kiruse@users.noreply.github.com>
2024-04-15 08:32:29 +0200

    optionality

--- src/validation.ts
+++ src/validation.ts
@@ -5,35 +5,38 @@ export function validate<T>(subject: any) {
 }
 
 class Inspector {
+  optional = false;
+
   constructor(public subject: any, public path?: string) {}
 
-  assert(condition: boolean | ((subject: any) => boolean), message: string) {
+  assert(condition: boolean | ((subject: any) => boolean), message = 'Assertion failed') {
     if (this.path) message = `${message} at ${this.path}`;
     if (typeof condition === 'function') condition = condition(this.subject);
     if (!condition) throw new ValidationError(message);
     return this;
   }
 
-  isString = () => this.assert(typeof this.subject === 'string', `Not a string`);
-  isBoolean = () => this.assert(typeof this.subject === 'boolean', `Not a boolean`);
-  isNumber = () => this.assert(typeof this.subject === 'number', `Not a number`);
-  isBigInt = () => this.assert(typeof this.subject === 'bigint', `Not a bigint`);
-  isSymbol = () => this.assert(typeof this.subject === 'symbol', `Not a symbol`);
-  isArray = () => this.assert(Array.isArray(this.subject), `Not an array`);
-  isObject = () => this.assert(typeof this.subject === 'object', `Not an object`);
-  isFunction = () => this.assert(typeof this.subject === 'function', `Not a function`);
-  isInstanceOf = (ctor: Ctor) => this.assert(this.subject instanceof ctor, `Not a ${ctor.name}`);
-  isOneOf = (values: any[]) => this.assert(values.includes(this.subject), `Expected one of ${values.join(', ')}`);
-  isNoneOf = (values: any[]) => this.assert(!values.includes(this.subject), `Expected none of ${values.join(', ')}`);
+  isOptional = () => { this.optional = true; return this; }
+  isString = () => this.assert(this.optional || typeof this.subject === 'string', `Not a string`);
+  isBoolean = () => this.assert(this.optional || typeof this.subject === 'boolean', `Not a boolean`);
+  isNumber = () => this.assert(this.optional || typeof this.subject === 'number', `Not a number`);
+  isBigInt = () => this.assert(this.optional || typeof this.subject === 'bigint', `Not a bigint`);
+  isSymbol = () => this.assert(this.optional || typeof this.subject === 'symbol', `Not a symbol`);
+  isArray = () => this.assert(this.optional || Array.isArray(this.subject), `Not an array`);
+  isObject = () => this.assert(this.optional || typeof this.subject === 'object', `Not an object`);
+  isFunction = () => this.assert(this.optional || typeof this.subject === 'function', `Not a function`);
+  isInstanceOf = (ctor: Ctor) => this.assert(this.optional || this.subject instanceof ctor, `Not a ${ctor.name}`);
+  isOneOf = (values: any[]) => this.assert(this.optional || values.includes(this.subject), `Expected one of ${values.join(', ')}`);
+  isNoneOf = (values: any[]) => this.assert(this.optional || !values.includes(this.subject), `Expected none of ${values.join(', ')}`);
   isTruthy = () => this.assert(this.subject, 'Not truthy');
   isFalsy = () => this.assert(!this.subject, 'Not falsy');
   isNull = () => this.assert(this.subject === null, 'Not null');
   isUndef = () => this.assert(this.subject === undefined, 'Not undefined');
   isDef = () => this.assert(this.subject !== undefined, 'Not defined');
-  eq = (value: any) => this.assert(this.subject === value, `Not equal to: ${value}`);
-  neq = (value: any) => this.assert(this.subject !== value, `Equal to: ${value}`);
-  is = (value: any) => this.assert(Object.is(this.subject, value), `Isn't: ${value}`);
-  isnt = (value: any) => this.assert(!Object.is(this.subject, value), `Is: ${value}`);
+  eq = (value: any) => this.assert(this.optional || this.subject === value, `Not equal to: ${value}`);
+  neq = (value: any) => this.assert(this.optional || this.subject !== value, `Equal to: ${value}`);
+  is = (value: any) => this.assert(this.optional || Object.is(this.subject, value), `Isn't: ${value}`);
+  isnt = (value: any) => this.assert(this.optional || !Object.is(this.subject, value), `Is: ${value}`);
 }
 
 export class Validator<T> extends Inspector {
@@ -47,7 +50,6 @@ export class Validator<T> extends Inspector {
   }
 
   at(keypath: PropertyKey[], callback: (val: Inspector) => void) {
-    console.log(Array.from(iterateDeep(this.subject, keypath)));
     for (const [path, value] of iterateDeep(this.subject, keypath)) {
       callback(new Inspector(value, stringifyKeyPath(path)));
     }
@@ -81,7 +83,7 @@ function* iterateDeep(obj: any, keypath: PropertyKey[], depth = 0): Generator<[P
       }
       return;
     } else {
-      obj = obj[key];
+      obj = obj?.[key];
     }
   }
   yield [keypath, obj];
